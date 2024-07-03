e0f6f871c0c3609b101a01ca6c810adb3d2033b0
trung <anzure1234@gmail.com>
2024-07-03 21:19:24 +0700

    123

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+
+import {ToogleTheme} from "@/components/toggletheme";
+
+const RegisterPage = () => {
+    return (<div>
+            <ToogleTheme/>
+            <div>Register page</div>
+        </div>
+    )
+}
+
+export default RegisterPage
